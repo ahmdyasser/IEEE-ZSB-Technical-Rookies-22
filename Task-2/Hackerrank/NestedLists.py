4e2c176fb092bd56974d0bfa830b0ce0434b6570
Ahmad Yasser <ahmadyas_ser@icloud.com>
2021-12-23 19:26:18 +0200

    fix spaces

--- Task-2/Hackerrank/NestedLists.py
+++ Task-2/Hackerrank/NestedLists.py
@@ -1,12 +1,14 @@
-    scores = []
-    names = []
-    sortedArr = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        scores.append(score)
-        names.append(name)
-        
+scores = []
+names = []
+sortedArr = []
+
+for _ in range(int(input())):
+    
+    name = input()
+    score = float(input())
+    scores.append(score)
+    names.append(name)
+
 s = set(scores) 
 s.remove(min(s))
 secMin = min(s)
@@ -14,7 +16,7 @@ secMin = min(s)
 for i in range(0, len(names)):
     if scores[i] == secMin:
         sortedArr.append(names[i])
-        
+
 sortedArr.sort()
 for i in range(0, len(sortedArr)):
     print(sortedArr[i])
